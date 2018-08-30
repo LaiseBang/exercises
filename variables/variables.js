65f8f8f7155e2a8fcfa766c20168e4fe59f80233
Laise Bang Henriksen <31646151+LaiseBang@users.noreply.github.com>
2018-08-30 10:26:42 +0200

    variables var let const

---
+++ variables/variables.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("variables");
+
+if (true == true) {
+  var oldStyle = "Peter";
+  let newStyle = "also Peter";
+}
+
+let newStyle = "Peter"; //declare a variable with the same name but different content, use let
+console.log(oldStyle);
+
+console.log(newStyle);
